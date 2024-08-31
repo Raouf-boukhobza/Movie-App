719dcb55b5187d549de2465fa612d324cb254ffb
raouf <raoufboukhobza3@gmail.com>
2024-08-31 23:30:56 +0100

    create the movie Api

---
+++ app/src/main/java/com/raouf/movieapp/data/remote/MovieApi.kt
@@ -0,0 +1,21 @@
+package com.raouf.movieapp.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieApi {
+
+    @GET("movie/{category}")
+    suspend fun getMovieData(
+        @Path("category") category: String,
+        @Query("page") page: Int,
+        @Query("Api_key") apiKey: String = ApiKey
+    )
+
+    companion object {
+        const val baseUrl = "https://api.themoviedb.org/3/"
+        const val image_BaseUrl = "https://image.tmdb.org/t/p/w500"
+        const val ApiKey = "05bde11ed9ddaf41959f5df9e43fa6a8"
+    }
+}
