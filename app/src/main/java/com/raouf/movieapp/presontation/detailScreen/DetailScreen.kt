0d36677a7a8f330f14d31d97b4e0e2c41bc80efc
raouf <raoufboukhobza3@gmail.com>
2024-09-18 14:35:05 +0100

    update the overview in detail screen

--- app/src/main/java/com/raouf/movieapp/presontation/detailScreen/DetailScreen.kt
+++ app/src/main/java/com/raouf/movieapp/presontation/detailScreen/DetailScreen.kt
@@ -1,7 +1,11 @@
+@file:Suppress("DEPRECATION")
+
 package com.raouf.movieapp.presontation.detailScreen
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,13 +18,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -36,9 +46,9 @@ import java.util.Locale
 @Composable
 fun DetailScreen(
     id: Int,
-    state : DetailScreenState,
+    state: DetailScreenState,
     onEvent: (DetailScreenEvent) -> Unit
-){
+) {
     onEvent(DetailScreenEvent.SelectMovie(id))
     val movie = state.selectedMovie
     movie?.let {
@@ -61,7 +71,7 @@ fun DetailScreen(
                 ).state
                 Box(
                     modifier = Modifier.background(color = Color.Black)
-                ){
+                ) {
                     TrailerPlayer(
                         videoKey = movie.videoUrl,
                         lifecycleOwner = LocalLifecycleOwner.current
@@ -142,17 +152,41 @@ fun DetailScreen(
                 fontWeight = FontWeight.Medium,
                 modifier = Modifier.padding(start = 12.dp)
             )
-            Text(
+            ExpandableText(
                 text = movie.overview,
-                fontSize = 16.sp,
-                color = Color.LightGray,
-                modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+                collapsedMaxLines = 3
             )
-
-
         }
 
     }
 
 
+}
+
+@Composable
+fun ExpandableText(text: String, collapsedMaxLines: Int = 3) {
+
+    var isExpanded by remember { mutableStateOf(false) }
+    Column(modifier = Modifier.padding(8.dp)){
+        Text(
+            text = text,
+            maxLines = if (isExpanded) Int.MAX_VALUE else collapsedMaxLines,
+            overflow = TextOverflow.Ellipsis,
+            color = Color.LightGray,
+            fontSize = 17.sp,
+            modifier = Modifier
+                .clickable { isExpanded = !isExpanded }
+                .animateContentSize() // This will animate the expansion and collapse
+        )
+
+       // Show a button for expanding/collapsing
+
+            Text(
+                if (isExpanded) "Show Less" else "Show More",
+                color = Color.Red,
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .clickable { isExpanded = !isExpanded }
+            )
+    }
 }
